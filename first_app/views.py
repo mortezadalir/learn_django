37faca8cb2d8df2a031534afcba7f34d444e189b
morteza dalirian <morteza.daliriyan@gmail.com>
2020-12-11 22:23:13 +0330

    model and modelform

--- first_app/views.py
+++ first_app/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
+from .forms import Personform
 
 # Create your views here.
 def index(request):
+    form=Personform()
+    if request.method=='POST':
+        form=Personform(request.POST)
+        form.save()
+        print(form)
+        if form.is_valid():
+            print(form.cleaned_data['first_name'])
+            print(form.cleaned_data['last_name'])
+        
     #return HttpResponse("<em>Hello World</em>")
-    return render(request,'first_app/index.html')
+    return render(request,'first_app/index.html',{'form':form})
     
     
 def myapp(request):
