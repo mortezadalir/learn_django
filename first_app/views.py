a7b588cad983d1e64c1864c7c612ad3b6e033dad
morteza dalirian <morteza.daliriyan@gmail.com>
2020-12-12 22:55:20 +0330

    template tags

--- first_app/views.py
+++ first_app/views.py
@@ -5,7 +5,9 @@ from .forms import Personform
 
 # Create your views here.
 def index(request):
+    name=['morteza','hosein','ali']
     form=Personform()
+    my_dict={'name':name,'form':form,'insert':"Hello THIS is Injected from views"}
     if request.method=='POST':
         form=Personform(request.POST)
         form.save()
@@ -15,7 +17,7 @@ def index(request):
             print(form.cleaned_data['last_name'])
         
     #return HttpResponse("<em>Hello World</em>")
-    return render(request,'first_app/index.html',{'form':form})
+    return render(request,'first_app/index.html',my_dict)
     
     
 def myapp(request):
